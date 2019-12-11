d28d15180f42f036695849a6965403dee02ef154
Maya Weissman <47795173+mweissman97@users.noreply.github.com>
2019-12-11 12:08:40 -0500

    Update macaronjava.js

--- rando/macaronjava.js
+++ rando/macaronjava.js
@@ -43,5 +43,6 @@ var allfill = [ganache, creamy, caramel, frozen, fruit]
 
 function macaron() {
   var numshell = Math.floor(Math.random() * (sweetshell.length));
-  document.getElementById('macdisplay').innerHTML = sweetshell[numshell);
+  var numfill = Math.floor(Math.random() * (creamy.length));
+  document.getElementById('macdisplay').innerHTML = sweetshell[numshell];
 }
